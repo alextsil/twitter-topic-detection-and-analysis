2f453c31f57b02a4b1a8b2530413c4fefb893103
alextsil <alex19924@gmail.com>
2017-06-04 15:43:44 +0300

    Hashtag term frequency

--- software/main.py
+++ software/main.py
@@ -18,9 +18,6 @@ def preprocess(s, lowercase=False):
 count = 0
 db = db()
 
-# text = "RT @Scavino45: President Trump pays respects and  delivers #MemorialDay " \
-#        "remarks at Arlington National Cemetery. https://t.co/D5rl948J6u"
-#
 regex_str = [
     r'<[^>]+>',  # HTML tags
     r'(?:@[\w_]+)',  # @-mentions
@@ -36,7 +33,7 @@ tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNOREC
 print("Token compilation completed")
 
 punctuation = list(string.punctuation)
-stop = stopwords.words('english') + punctuation + ['rt', 'via']
+stop = stopwords.words('english') + punctuation + ['rt', 'via', '…']
 print("Stopword list construction completed")
 
 allTweets = db.getAll()
@@ -44,12 +41,16 @@ allTweets = db.getAll()
 count_all = Counter()
 for tweet in allTweets:
     tweetText = tweet['text'].lower()
-    # Create a list with all the terms
-    terms_stop = [term for term in preprocess(tweetText) if term not in stop]
+    # Create a list with all the terms - Enalagi metaksi terms_
+    # terms_stop = [term for term in preprocess(tweetText) if term not in stop]
+    # terms_bigram = bigrams(terms_stop)
+
+    terms_hash = [term for term in preprocess(tweetText)
+                  if term.startswith('#')]
     # Update the counter
-    count_all.update(terms_stop)
+    count_all.update(terms_hash)
     count += 1
     print("\rLoop run " + str(count) + " times", end="")
 
-# Print the first 30 most frequent words
-print(count_all.most_common(30))
+# Print the first 100 most frequent words
+print(count_all.most_common(100))
