8adf94a2979d32f1a51adafb5848eeb3d605d0c0
Sotiris Karapostolakis <sotkar14@hotmail.com>
2017-06-10 18:49:37 +0300

    Geoprediction for 200 latest tweets: Deploy 1

--- software/main.py
+++ software/main.py
@@ -12,10 +12,7 @@ a = gp.dictGeoGenerator(geotagged)
 
 b = list(a)
 
-qa.getUserTimeLine(str(b[0]))
-qa.getUserTimeLine(str(b[1]))
-
 #gp.printGeotaggedCities(a)
 
-#for key in b:
-#	qa.getUserTimeLine(str(key))
+for key in b:
+	qa.getUserTimeLine(str(key))
